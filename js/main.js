0b897ca155cce302a685fe9e042bcee72c4191c5
therealsult <113542978+therealsult@users.noreply.github.com>
2023-11-13 06:09:57 -0500

    added some more functions

--- js/main.js
+++ js/main.js
@@ -79,8 +79,6 @@ function render() {
     }
   });
 
-  //Array with content for the earbuds hotspots
-
 const infoBoxes = [
   {
     title: "Comfortable EarBud Tips",
@@ -184,6 +182,61 @@ const infoBoxes = [
 
 
   button.addEventListener("click", hamburgerMenu, false);	
+
+
+
+// intro
+
+var s = skrollr.init();
+
+
+
+
+
+
+  // xray-slider
+
+  (function(){
+    "use strict";
+
+
+var imageCon = document.querySelector('#imageCon'),
+    drag = document.querySelector('.image-drag'),
+    left = document.querySelector('.image-left'),
+    dragging = false,
+    min = 0,
+    max = imageCon.offsetWidth;
+
+function onDown() {
+  dragging = true;
+}
+
+function onUp() {
+  dragging = false;
+}
+
+function onMove(event) {
+  if(dragging===true) {
+    var x = event.clientX - imageCon.getBoundingClientRect().left;
+    console.log(event.clientX);
+    console.log(imageCon.getBoundingClientRect().left);
+
+    if(x < min) { 
+      x = min;    
+    }
+   else if(x > max) { 
+      x = max-4; 
+    }
+    drag.style.left = x + 'px';
+    left.style.width = x + 'px';
+  }
+}
+
+drag.addEventListener('mousedown', onDown, false); 
+document.body.addEventListener('mouseup', onUp, false);
+document.body.addEventListener('mousemove', onMove, false);
+
+})();
 
 
 
